b0a7ceb53b00dddff5cb458f6500bd0f92e90c42
Sean Melody <seandmelody@gmail.com>
2021-02-03 19:08:49 -0800

    Comments added to JavaScript page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
+// JavaScript File to show and hide the into gif
+
+// Consts to access the divs
 const welcome = document.querySelector("#welcome")
-
 const backgroundMain = document.querySelector(".background-main")
-
-backgroundMain.classList.add("background-display")
-
 const homepage = document.querySelector(".homepage")
 
+// Add the classes to show/hide
+backgroundMain.classList.add("background-display")
 homepage.classList.add("background-hide")
 
+// Onclick event listener to hide the gif when clicked, and show the rest of the page
 welcome.addEventListener("click", (e) => {
     e.preventDefault()
 
+    // Add/Remove Classes
     backgroundMain.classList.add("background-hide")
     backgroundMain.classList.remove("background-display")
 
+    // Add/Remove Classes
     homepage.classList.remove("background-hide")
     homepage.classList.add("background-display")
 
